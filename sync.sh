b8e54c8d59e53d1ec8e0c941e8182081c187e914
PapaAnam <hairulanam21@outlook.com>
2018-06-14 09:23:45 +0700

    awal lagi

--- sync.sh
+++ sync.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-mv public_html/index.php index.php.saved
+mv -f laravel_funzy/index.php.saved public_html/index.php
 rm -rf public_html/*
 # update project/ to your directory name
 cp -a project/public/* public_html
 cp project/public/.* public_html
 rm -rf public_html/index.php
-mv index.php.saved public_html/index.php
 cp link.sh public_html/link.sh
 ./public_html/link.sh
 rm public_html/link.sh
