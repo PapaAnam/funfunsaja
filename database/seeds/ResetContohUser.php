b05d295cb38c8eb8008f3733f5c22b6f0c441975
PapaAnam <hairulanam21@outlook.com>
2018-07-03 11:06:43 +0700

    mm

---
+++ database/seeds/ResetContohUser.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class ResetContohUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	User::updateOrCreate([
+    		'email'=>'hairulanam21@outlook.com'
+        ], [
+           'password'=>bcrypt('Narutolover11'),
+           'wrong_login'=>0,
+           'verification_url'=>null,
+           'token_number'=>null,
+           'phone_number'=>'085322778935',
+           'avatar'=>'ujicoba',
+           'balance'=>0,
+           'point'=>0,
+           'status'=>'1',
+           'username'=>'Anamcoollzz',
+           'web'=>'http://anamkun.com',
+           'description'=>'Aku hanyalah seorang laki-laki',
+       ]);
+        echo 'user anam terreset';
+    }
+}
