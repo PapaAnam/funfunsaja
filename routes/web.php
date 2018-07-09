7c0fbc9304d41ed909055849fcb4e7c0717a66dd
PapaAnam <hairulanam21@outlook.com>
2018-07-09 13:48:40 +0700

    coba lagi

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,32 @@
 // });
 
 # USER PROFILE TANPA LOGIN
+Route::get('/sms-setting', function(){
+	$sms = Setting::sms();
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => config('sms.api'),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => "[  " . json_encode([
+                'phone_number'=>'085322778935',
+                'message'=>'hjfghjg gfjgf hfjhgf',
+                'device_id'=>$sms['SMS_ME_DEVICE']
+            ]) . "]",
+            CURLOPT_HTTPHEADER => array(
+                "authorization: ".config('sms.token'),
+                "cache-control: no-cache"
+            ),
+        ));
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $response = json_decode(curl_exec($curl),true);
+        curl_close($curl);
+        dd($response);
+});
 Route::get('/profile/{username}', 'ListUserController@profile');
 
 Route::get('/clear-sliders', 'Admin\SliderController@clear');
