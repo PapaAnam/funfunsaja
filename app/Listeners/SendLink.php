e3ffd07dd3c4d724e541119a2086a973b3d1f99e
PapaAnam <hairulanam21@outlook.com>
2018-07-12 16:35:30 +0700

    resetcontohuser

--- app/Listeners/SendLink.php
+++ app/Listeners/SendLink.php
@@ -5,13 +5,13 @@ namespace App\Listeners;
 use App\Events\SendLinkAndSms;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use App\Mail\SendLink;
+use App\Mail\SendLink as SendLinkMail;
 
 class SendLink
 {
 
     public function handle(SendLinkAndSms $event)
     {
-        Mail::to($event->user->email)->send(new SendLink($event->user, $event->email_type));
+        Mail::to($event->user->email)->send(new SendLinkMail($event->user, $event->email_type));
     }
 }
